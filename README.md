f870f0d899f78174bec1456728e72b7dc4919ad0
Gray Hamilton <grayhamilton@gmail.com>
2018-07-22 17:32:20 -0500

    Added screenshots

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
+# Leaflet Map Starter
+ 
+ A starter app for Ionic development demonstrating the use of Leaflet, an open-source JavaScript library for mobile-friendly interactive maps.
+
+ This project is based on the Ionic's Blank starter template, and uses Ionic 3.6.0 and Leaflet 1.1.0.
+
+ Features Include:
+ 
+* Zoom control
+* Location tracking control
+* Add markers with custom label text using Ionic Alerts
+* Base layer control with 6 presets
+    - Streets
+    - Dark
+    - Traffic Day
+    - Traffic Night
+    - Topo "Outdoors"
+    - Satellite/Streets hybrid
+
+Dependencies and their required install/add commands:
+
+* Leaflet: 
+    - npm install leaflet --save
+    - npm install @types/leaflet --save
+* Leaflet locate control:
+    - npm install leaflet.locatecontrol --save
+    - npm install @types/leaflet.locatecontrol --save
+* Network: 
+    - npm install @ionic-native/network --save
+    - ionic cordova plugin add cordova-plugin-network-information
+* Geolocation:
+    - npm install @ionic-native/geolocation --save
+    - ionic cordova plugin add cordova-plugin-geolocation
+    
+
+
+## This project is based on the Blank starter template using Ionic 3.6.0 
+
 This is a starter template for [Ionic](http://ionicframework.com/docs/) projects.
 
 ## How to use this template
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,110 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ViewController, Platform, AlertController } from 'ionic-angular';
+import { Network } from '@ionic-native/network';
+import { Geolocation } from '@ionic-native/geolocation';
+import 'leaflet';
+import 'leaflet.locatecontrol';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private viewCtrl: ViewController, public platform: Platform, public network: Network, public geolocation: Geolocation, public alertCtrl: AlertController) {
 
   }
 
-}
+  ionViewDidLoad() {
+    if (this.platform.platforms[0] == 'browser') {
+      this.checkConnection();
+    } else {
+      this.loadMap();
+    }
+  }
+
+
+  checkConnection() {
+    let t = this;
+
+    this.platform.ready().then(() => {
+
+      let networkType = this.network.type;
+
+      if (networkType == "unknown" || networkType == "none" || networkType == undefined) {
+        let alert = this.alertCtrl.create({
+          title: 'Not online',
+          subTitle: 'Connect to the internet and try again.',
+          buttons: [
+            {
+              text: "Try again",
+              handler: function () { t.checkConnection() }
+            }
+          ]
+        });
+        alert.present();
+
+      } else {
+        this.loadMap();
+      }
+
+    });
+
+  }
+
+  loadMap() {
+
+    let lyrStreets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JheWhhbWlsdG9uIiwiYSI6ImNpbHFxZmFpNzA4bW52dmx5eTY5YW5ia2EifQ.hoCUaTVuAJqLlSchwzwAGQ");
+    let lyrDark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JheWhhbWlsdG9uIiwiYSI6ImNpbHFxZmFpNzA4bW52dmx5eTY5YW5ia2EifQ.hoCUaTVuAJqLlSchwzwAGQ");
+    let lyrTrafficDay = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/traffic-day-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JheWhhbWlsdG9uIiwiYSI6ImNpbHFxZmFpNzA4bW52dmx5eTY5YW5ia2EifQ.hoCUaTVuAJqLlSchwzwAGQ");
+    let lyrTrafficNight = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/traffic-night-v2/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JheWhhbWlsdG9uIiwiYSI6ImNpbHFxZmFpNzA4bW52dmx5eTY5YW5ia2EifQ.hoCUaTVuAJqLlSchwzwAGQ");
+    let lyrOutdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JheWhhbWlsdG9uIiwiYSI6ImNpbHFxZmFpNzA4bW52dmx5eTY5YW5ia2EifQ.hoCUaTVuAJqLlSchwzwAGQ");
+    let lyrSatelliteStreets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3JheWhhbWlsdG9uIiwiYSI6ImNpbHFxZmFpNzA4bW52dmx5eTY5YW5ia2EifQ.hoCUaTVuAJqLlSchwzwAGQ");
+
+    let map = L.map('map',
+      {
+        layers: [lyrStreets, lyrDark, lyrTrafficDay, lyrTrafficNight, lyrOutdoors, lyrSatelliteStreets]
+      }
+    );
+
+    var baseMaps = {
+      "Outdoors": lyrOutdoors,
+      "Satellite Streets": lyrSatelliteStreets,
+      "Traffic (Day)": lyrTrafficDay,
+      "Traffic (Night)": lyrTrafficNight,
+      "Day": lyrStreets,
+      "Night": lyrDark,
+    }
+
+    let layerCtrl = L.control.layers(baseMaps).addTo(map);
+
+    L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token==pk.eyJ1IjoiZ3JheWhhbWlsdG9uIiwiYSI6ImNpbHFxZmFpNzA4bW52dmx5eTY5YW5ia2EifQ.hoCUaTVuAJqLlSchwzwAGQ", {
+      //attribution: '',
+      //maxZoom: 15
+    }).addTo(map);
+
+    L.control.locate({
+      flyTo: true,
+      setView: "always",
+      strings: {
+        title: "Locate me"
+      },
+      markerStyle: {
+        color: "White",
+        fillColor: "Green"
+      },
+      circleStyle: {
+        color: "White",
+        fillColor: "DodgerBlue"
+      }
+    }).addTo(map);
+
+    //web location
+    map.locate({ setView: true, });
+
+  } //end loadMap
+
+
+
+} //end HomePage class
